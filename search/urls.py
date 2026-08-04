cde96efb5023b3b3b48a858dd4e3e133c155218e
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-08-04 15:00:00 +0200

    database-modelisation(django): add logic to search a product, find a better one and save it to the user count

--- search/urls.py
+++ search/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'search'
+
 urlpatterns = [
-  path('', views.index, name='index')
+  path('', views.index, name='index'),
+  path('recherche/', views.recherche_produit, name='recherche_produit'),
 ]
