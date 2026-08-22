fb18c778bb8c976a82e7c33b468c10fa906f1ef6
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-08-22 20:08:41 +0200

    feat(test): start implementing end-to-end tests for a user journey

---
+++ foods/tests/end_to_end_tests.py
@@ -0,0 +1,25 @@
+from playwright.sync_api import Page, expect
+
+def test_homepage(page: Page):
+  page.goto("http://127.0.0.1:8000")
+
+  expect(page).to_have_title("Nutrifood")
+
+def test_homepage_content(page: Page):
+  page.goto("http://127.0.0.1:8000")
+
+  expect(page.locator("h1")).to_have_text("Bienvenue sur Nutrifood")
+  expect(page.get_by_label("Nom du produit")).to_be_empty()
+  expect(page.get_by_role("link", name="Trouver une meilleure alternative")).to_have_attribute("href", "/alternative/")
+
+def test_rechercher_produit(page: Page):
+  page.goto("http://127.0.0.1:8000/")
+
+  page.get_by_label("Nom du produit").fill("Coca-Cola")
+  page.get_by_role("button", name="Rechercher").click()
+
+  expect(page).to_have_url("http://127.0.0.1:8000/recherche/?q=Coca-Cola"
+  )
+
+  expect(page.locator("h1")).to_have_text('Résultats de la recherche pour "Coca-Cola" ')
+
